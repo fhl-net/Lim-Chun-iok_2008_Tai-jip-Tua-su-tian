565ed852cdb23a053c61feee795369ac72281116
薛丞宏 <ihcaoe@gmail.com>
2018-06-01 22:07:58 +0800

    ka 2018/05/16 e pan-pun

--- sql2csv.sh
+++ sql2csv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # The̍h-tiāu ip
-echo "select id,word,other,chinese,exp,example,username,modtime,han,english,page from dic_can;" | \
+echo "select id,word,other,chinese,exp,example,username,modtime,han,english,page from dic;" | \
   cat $1 - | \
-  sed '/CREATE INDEX work_dic_can ON dic_can USING btree (word);/d' | \
+  sed '/CREATE INDEX .* ON /d' | \
   sed '/ALTER TABLE/{N;d;}' | \
   sqlite3 -header -csv | \
   cat > dict.csv
