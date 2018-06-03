f6e0344161f7cb1a0435c2042a28dadabe661583
薛丞宏 <ihcaoe@gmail.com>
2018-06-03 08:43:02 +0800

    2018/05/16 e csv chiau id pai，i-au chiah-ho khuann cha-piat

--- sql2csv.sh
+++ sql2csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The̍h-tiāu ip
-echo "select id,word,other,chinese,exp,example,username,modtime,han,english,page from dic;" | \
+echo "SELECT id,word,other,chinese,exp,example,username,modtime,han,english,page From dic ORDER BY id;" | \
   cat $1 - | \
   sed '/CREATE INDEX .* ON /d' | \
   sed '/ALTER TABLE/{N;d;}' | \
